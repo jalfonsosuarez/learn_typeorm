235b86cbdbf1291d0f1447f459c8ad7de05b044a
= <=>
2022-06-13 08:21:47 +0200

    Swagger apply

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsBoolean,
   IsNotEmpty,
@@ -10,19 +11,23 @@ import {
 
 @Injectable()
 export class CreateTaskDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   description: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsNumber()
   @IsPositive()
   user: number;
 
+  @ApiProperty()
   @IsOptional()
   @IsNotEmpty()
   @IsBoolean()
